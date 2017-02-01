8ff7f6fbcb11947038b5f7d73f7c027e0cca83f4
Louis <louis@louisede-macbook-air.local>
2017-02-02 00:03:50 +0800

    [Add] 實際顯示存在firebase資料庫的會員資料

--- NTU-Tutor/AppDelegate.swift
+++ NTU-Tutor/AppDelegate.swift
@@ -17,7 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         FIRApp.configure()
         let strSrcPropertyListPath = Bundle.main.path(forResource: "AppStringTable", ofType: "plist")
--- NTU-Tutor/TeacherListView.swift
+++ NTU-Tutor/TeacherListView.swift
@@ -7,16 +7,29 @@
 //
 
 import UIKit
+import Firebase
 
 class TeacherListView: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var Toolbar: UIToolbar!
     @IBOutlet weak var TeacherTable: UITableView!
     
+    var AllTeacherData: NSArray?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let nib = UINib( nibName: "UserTableViewCell", bundle:nil )
         TeacherTable.register( nib, forCellReuseIdentifier: "Cell" )
+        
+        FirebaseDatabaseRef.child("Users/").observeSingleEvent(of: .value, with: {
+            (snapshot) in
+            // Get user value
+            let allUserData = snapshot.value as? NSDictionary //[uid,data]
+            self.AllTeacherData = allUserData?.allValues as NSArray?
+            self.TeacherTable.reloadData()
+        }){ (error) in
+            print(error.localizedDescription)
+        }
     }
     
     func numberOfSections(in tableView: UITableView) -> Int
@@ -28,30 +41,24 @@ class TeacherListView: UIViewController, UITableViewDelegate, UITableViewDataSou
     {
         //let userID = FIRAuth.auth()?.currentUser?.uid
         // need to implement from Firbase database
-        return 3
+        if( AllTeacherData != nil ){
+            return AllTeacherData!.count
+        }
+        return 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! UserTableViewCell
         
-        switch indexPath.row {
-        case 0:
-            cell.UserNameLabel.text = "Teacher 0"
-            cell.UserSexLabel.text = "男"
-            break
-        case 1:
-            cell.UserNameLabel.text = "Teacher 1"
-            cell.UserSexLabel.text = "女"
-            break
-        case 2:
-            cell.UserNameLabel.text = "Teacher 2"
-            cell.UserSexLabel.text = "男"
-            break
-        default:
-            break
+        if( AllTeacherData != nil ){
+            if let teacherData = AllTeacherData![ indexPath.row ] as? NSDictionary{
+                cell.UserNameLabel.text = teacherData.value(forKey: "UserName") as! String?
+                cell.UserSexLabel.text = teacherData.value(forKey: "Sex") as! String?
+            }
         }
         
         return cell
     }
+    
 
 }
