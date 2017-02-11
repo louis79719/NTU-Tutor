5ffca0f0aa261f0152915433e4dec12a6640a8fb
louis719 <louis79719@gmail.com>
2017-02-11 23:31:58 +0800

    [Add] Modify Password 功能

--- NTU-Tutor/EditAccountViewController.swift
+++ NTU-Tutor/EditAccountViewController.swift
@@ -7,16 +7,41 @@
 //
 
 import UIKit
+import Firebase
 
 class EditAccountViewController: UIViewController {
 
+    var currentUser : FIRUser? = nil
+    
+    @IBOutlet weak var PasswordText: UITextField!
+    @IBOutlet weak var PasswordAgainText: UITextField!
+    @IBOutlet weak var EditOkButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        currentUser = FIRAuth.auth()?.currentUser
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    
+    @IBAction func OnEditOkButtonClicked(_ sender: Any) {
+        if( PasswordText.text == PasswordAgainText.text ){
+            currentUser?.updatePassword(PasswordText.text!){
+                (error) in
+                if( error != nil ){
+                    ShowErrorAlert( view: self, title: "Update Password fail", message: error!.localizedDescription)
+                }
+                else{
+                    self.performSegue(withIdentifier: "SegueToAccountPage", sender: self)
+                }
+            }
+        }
+        else{
+            ShowErrorAlert( view: self, title: "Fail", message: "Enter the same password twice.")
+        }
     }
 
 }
